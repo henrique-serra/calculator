cf7f13267d31f236258d9b9ce2f1cfed835ffbb5
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2024-12-18 13:23:46 +0000

    Add calculation functions

--- app.js
+++ app.js
@@ -2,6 +2,46 @@ const screen = document.querySelector("#screen")
 const acBtn = document.querySelector("#acBtn");
 const numeralBtns = document.querySelectorAll(".numeralBtn");
 
+function isString(x) {
+    if (typeof x === "string") return Number(x);
+    else if (typeof x === "number") return x;
+    return;
+}
+
+function add(a, b) {
+    a = isString(a);
+    b = isString(b);
+    return a + b;
+}
+
+function subtract(a, b) {
+    a = isString(a);
+    b = isString(b);
+    return a - b;
+}
+
+function multiply(a, b) {
+    a = isString(a);
+    b = isString(b);
+    return a * b;
+}
+
+function divide(a, b) {
+    a = isString(a);
+    b = isString(b);
+    return a / b;
+}
+
+function calculateOperation(a, b, operator) {
+    const methods = {
+        "+": add,
+        "-": subtract,
+        "*": multiply,
+        "/": divide,
+    }
+    return methods[operator](a, b);
+}
+
 acBtn.addEventListener("click", () => {
     screen.textContent = "";
 })
